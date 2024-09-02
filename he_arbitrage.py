ca1264653fa91e7f92ed3643fe75564031f9f8d9
cheery22487 <135150180+Cheery22487@users.noreply.github.com>
2024-09-02 19:28:26 +0200

    test

--- he_arbitrage.py
+++ he_arbitrage.py
@@ -24,6 +24,8 @@ tokenlist = ["DEC","SPS","BEE","SWAP.USDT","SWAP.BTC","VOUCHER","SWAP.DOGE","SWA
 #bannedpools = ["DEC:SUFB","SWAP.HIVE:SWAP.WAX","BEE:SWAP.WAX","SPS:SWAP.WAX","SPS:SWAP.SLP","SWAP.AXS:SWAP.SLP","SPS:SWAP.AXS","SPS:SWAP.GALA","SPS:FSPS","STARBITS:SWAP.HBD","DEC:VOID","SWAP.HIVE:SEED","SWAP.HBD:SEED"]
 bannedpools = []
 
+testasfdsg = 1
+
 def get_hive_balances(token,name):
     try:
         url = rpc + "/contracts"
